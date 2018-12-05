6c4d221da1bb901dc7e8d6cd7672a533c3ac85ea
Mickey Chen <poverwhelming@gmail.com>
2018-12-06 01:45:34 +0800

    added default language and installation

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,2 +1,1 @@
 source /usr/local/opt/asdf/asdf.sh
-
--- asdf/install.sh
+++ asdf/install.sh
@@ -10,11 +10,17 @@ asdf plugin-add elixir https://github.com/HashNuke/asdf-elixir.git
 echo "Install ASDF Elixir Plugin"
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 
-echo "Install ASDF Elixir Plugin with keyring"
+echo "Install ASDF Nodejs Plugin with keyring"
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-bash /usr/local/opt/asdf/plugins/nodejs/bin/import-release-team-keyring
+echo "Install Ruby Plugin"
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 
-echo "Finished getting ASDF language plugins"
+echo "Install and set global"
+cd ~
+asdf install
+cd -
+
 
 exit 0
