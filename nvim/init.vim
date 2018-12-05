fd25c85aa74269e963ec6368038ccf256454bda4
Mickey Chen <poverwhelming@gmail.com>
2018-12-06 02:57:36 +0800

    got package installing, made nvim/install.sh more flexible

--- nvim/init.vim
+++ nvim/init.vim
@@ -1,15 +1,23 @@
-syntax on
+" Start vim plug
+call plug#begin('~/.config/nvim/plugged')
 
-" Wrap gitcommit file types at the appropriate length
-filetype indent plugin on
-
-Plug 'usr/local/opt/fzf'
+" General plugs
+Plug '/usr/local/opt/fzf'
 Plug 'junegunn/fzf.vim'
 Plug 'scrooloose/nerdtree'
 Plug 'Xuyuanp/nerdtree-git-plugin'
 Plug 'vim-airline/vim-airline'
 Plug 'vim-airline/vim-airline-themes'
 
+call plug#end()
+
 " Airline
-let g:airline_theme="solarized"
+let g:airline_theme='solarized'
 let g:airline_solarized_bg='dark'
+
+" Syntax highlighting
+syntax on
+
+" Wrap gitcommit file types at the appropriate length
+filetype indent plugin on
+
--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-VIMHOME=~/.config/nvim
-VIM_DIR=$(printf "%s/nvim" $(pwd))
+NVIM_HOME=~/.config/nvim
+NVIM_DIR=$(printf "%s/nvim" $(pwd))
+
+echo "$NVIM_DIR"
 
 warn() {
     echo "$1" >&2
@@ -11,19 +13,23 @@ die() {
     exit 1
 }
 
-if [[ -d "$VIMHOME" ]] 
+if [[ -d "$NVIM_HOME" ]] 
 then 
-  die "$VIMHOME directory exists, you might want to back it up."
+  if [[ ! -L "$NVIM_HOME" ]]
+  then
+    die "$NVIM_HOME directory exists, you might want to back it up."
+  fi
+else
+  # Link folder and files
+  cd ~
+  mkdir -p ~/.config
+  ln -sf $NVIM_DIR $NVIM_HOME
 fi
-# Link folder and files
-cd ~
 
-mkdir -p ~/.config
-ln -sf $VIM_DIR $VIMHOME
-
-cd $VIMHOME
-curl -fLo $VIMHOME/autoload/plug.vim --create-dirs \
+cd $NVIM_HOME
+echo "Install Vim Plug"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -n -c "set nomore" +PlugUpgrade +PlugInstall +qall
+nvim -n -c "set nomore" +PlugUpgrade +PlugInstall +qall
 
 echo "Vim is ready to use. Happy Hacking～"
